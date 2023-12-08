71e55c810010ad440c21b52d5070f9aa7501d33f
Faisal Alkhashib <62709521+DirDanton@users.noreply.github.com>
2023-12-09 00:38:17 +0800

    x

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import json
 import random
 from log import *
 from settings import *
+#import confessions
 
 welcomeMsgs = []
 
@@ -11,6 +14,7 @@ with open('welcome.json', 'r+') as f:
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        await self.tree.sync()
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
@@ -22,11 +26,12 @@ class MyClient(discord.Client):
                     await message.add_reaction(v[value])
                 return
         if message.channel.id == settings()['general_chat']:
-            if random.randint(1,100) <= 10:
+            print(self.user.id)
+            if random.randint(1,100) <= 2 and not message.author.id == self.user.id:
                 await message.channel.send(settings()['random_chats'][random.randint(0,len(settings()['random_chats'])-1)])
     async def on_member_join(self, member):
         log('serverjoin.log', f'member <@{member.id}>')
-        channel = self.get_channel(1180901512222875728)
+        channel = self.get_channel(1142913771417915535)
         await channel.send(welcomeMsgs[random.randint(0,len(welcomeMsgs)-1)].replace("member", f"<@{member.id}>"))
 
 intents = discord.Intents.default()
@@ -34,4 +39,4 @@ intents.message_content = True
 intents.members = True
 
 client = MyClient(intents=intents)
-client.run('MTE4MDc5NDQ1NzY1OTAyMzM4MA.GcqvnB.AAhPLOZ_y-bUIf8Oome967pAdtJ5bCPFPnjjRQ')
+client.run('') # Currently using red reddington
